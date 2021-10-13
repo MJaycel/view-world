ea8fa781dd91658b92dfb957f9ab8f9f2213b6be
Mary Jaycel <skylrjx@gmail.com>
2021-10-13 01:16:39 +0100

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <WorldViewer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WorldViewer from '@/components/WorldViewer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WorldViewer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/WorldViewer.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+      <!-- <div v-for="country in countries" :key="country.tld">
+          <h3> {{country.name.official}} </h3>
+      </div> -->
+
+    <b-card-group columns>
+            <b-card
+                v-for="country in countries" 
+                :key="country.tld"
+                :img-src="country.flags.png"
+                :img-alt="country.name.official" 
+            >
+                <b-card-text>
+                    <h3> {{country.name.official}} </h3>
+                </b-card-text>
+            </b-card>
+        </b-card-group > 
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+const REST_URL = "https://restcountries.com/v3.1/all";
+
+    export default{
+        name: 'GiphyViewer',
+        data(){
+            return{
+                countries:[],
+                term: ""            
+            };
+        },
+        mounted() {
+            axios.get(`${REST_URL}`)
+            .then((response) => {
+                console.log(response.data)
+                this.countries = response.data
+                console.log(this.countries)
+            })
+            .catch(error => console.log(error))
+
+            // this.term = ""
+        },
+        methods:{
+
+        }
+    }
+
+</script>
+
+<style>
+@media (min-width: 34em) {
+    .card-columns {
+        -webkit-column-count: 2;
+        -moz-column-count: 2;
+        column-count: 2;
+    }
+}
+@media (min-width: 48em) {
+    .card-columns {
+        -webkit-column-count: 3;
+        -moz-column-count: 3;
+        column-count: 3;
+    }
+}
+@media (min-width: 62em) {
+    .card-columns {
+        -webkit-column-count: 4;
+        -moz-column-count: 4;
+        column-count: 4;
+    }
+}
+@media (min-width: 75em) {
+    .card-columns {
+        -webkit-column-count: 5;
+        -moz-column-count: 5;
+        column-count: 5;
+    }
+}
+</style>
